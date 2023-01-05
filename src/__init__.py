0786c411f7d1b530b5ab0ef1aac14d2b9271cbe1
karachi david <karachidavid31@gmail.com>
2023-01-05 16:07:39 +0100

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -32,6 +32,8 @@ def create_app(test_config=None):
 
     db.app = app
     db.init_app(app)
+    with app.app_context():
+    db.create_all()
 
     JWTManager(app)
     app.register_blueprint(auth)
